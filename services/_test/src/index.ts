e48e0683e0a5d3fb8a4e65412a35bfbf19f653a0
kbkk <6276426+kbkk@users.noreply.github.com>
2021-02-09 21:47:03 +0100

    use nest.js

--- services/_test/src/index.ts
+++ services/_test/src/index.ts
@@ -1,15 +1,7 @@
-import {Server, IncomingMessage, ServerResponse} from 'http';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './AppModule';
 
-import * as fastify from 'fastify';
-
-type FastifyServer = fastify.FastifyInstance<Server, IncomingMessage, ServerResponse>;
-
-export function createServer(): FastifyServer {
-    const server: FastifyServer = fastify({});
-
-    server.get('/', async (request, reply) => {
-        return {message: 'Hello!'};
-    });
-
-    return server;
-}
+(async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+})();
