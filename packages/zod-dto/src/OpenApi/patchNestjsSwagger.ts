1b1408cfb8c218f986e5ad030831b42e660e97c0
kbkk <6276426+kbkk@users.noreply.github.com>
2021-02-20 17:04:59 +0100

    feature(zod-dto): Add @nestjs/swagger patcher

---
+++ packages/zod-dto/src/OpenApi/patchNestjsSwagger.ts
@@ -0,0 +1,29 @@
+import { zodTypeToOpenApi } from './zodTypeToOpenApi';
+
+export const patchNestjsSwagger = (
+    schemaObjectFactoryModule = require('@nestjs/swagger/dist/services/schema-object-factory'),
+): void => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires,@typescript-eslint/naming-convention
+    const { SchemaObjectFactory } = schemaObjectFactoryModule;
+
+    const orgExploreModelSchema = SchemaObjectFactory.prototype.exploreModelSchema;
+
+    SchemaObjectFactory.prototype.exploreModelSchema = function(type, schemas, schemaRefsStack) {
+        if (this.isLazyTypeFunc(type)) {
+            // eslint-disable-next-line @typescript-eslint/ban-types
+            type = (type as Function)();
+        }
+
+        if(!type.zodSchema) {
+            return orgExploreModelSchema(type, schemas, schemaRefsStack);
+        }
+
+        const openApiDef = zodTypeToOpenApi(type.zodSchema);
+
+        schemas.push({
+            [type.name]: openApiDef,
+        });
+
+        return type.name;
+    };
+};
