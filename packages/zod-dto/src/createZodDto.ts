9c0cbdb82383e0e593160cbf7dee5a1efbd124e6
kbkk <6276426+kbkk@users.noreply.github.com>
2021-02-27 19:09:47 +0100

    feature(zod-dto): Expose DTO.create method

--- packages/zod-dto/src/createZodDto.ts
+++ packages/zod-dto/src/createZodDto.ts
@@ -3,11 +3,16 @@ import * as z from 'zod';
 export type ZodDtoStatic<T> = {
     new (): T;
     zodSchema: z.ZodType<unknown>;
+    create(input: unknown): T;
 };
 
 export const createZodDto = <T extends z.ZodType<unknown>>(zodSchema: T): ZodDtoStatic<z.infer<T>> => {
     class SchemaHolderClass {
         public static zodSchema = zodSchema;
+
+        public static create(input: unknown): T {
+            return this.zodSchema.parse(input) as T;
+        }
     }
 
     return SchemaHolderClass;
