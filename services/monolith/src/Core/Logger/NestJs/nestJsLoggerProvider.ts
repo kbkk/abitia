4ad2f7eac1c9f4b05e81ecc282b7ea01dc362f51
kbkk <6276426+kbkk@users.noreply.github.com>
2021-03-14 19:33:31 +0100

    tests(monolith-service): test pino serialization

--- services/monolith/src/Core/Logger/NestJs/nestJsLoggerProvider.ts
+++ services/monolith/src/Core/Logger/NestJs/nestJsLoggerProvider.ts
@@ -1,7 +1,5 @@
-import * as pinoFactory from 'pino';
-import { err } from 'pino-std-serializers';
-
 import { PinoLogger } from '../PinoLogger';
+import { pinoFactory } from '../pinoFactory';
 
 export const LOGGER = 'LOGGER';
 export const PINO_INSTANCE = 'PINO_INSTANCE';
@@ -16,18 +14,6 @@ export const nestJsLoggerProvider = [
     },
     {
         provide: PINO_INSTANCE,
-        useFactory: () => {
-            const isProd = process.env.NODE_ENV === 'production';
-            const pino = pinoFactory({
-                ...isProd ? {} : { prettyPrint: true },
-                serializers: {
-                    // only keys named 'err' and 'error' will be passed to the error serializer
-                    err,
-                    error: err,
-                },
-            });
-
-            return pino;
-        },
+        useFactory: () => pinoFactory(),
     },
 ];
