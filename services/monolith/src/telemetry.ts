7d7bab71e84633db4b30c88a5d68fdb252052ecf
kbkk <6276426+kbkk@users.noreply.github.com>
2021-03-20 18:30:23 +0100

    feature(monolith-service): add zipkin exporter

--- services/monolith/src/telemetry.ts
+++ services/monolith/src/telemetry.ts
@@ -1,13 +1,23 @@
 import { context, trace } from '@opentelemetry/api';
 import { AsyncHooksContextManager } from '@opentelemetry/context-async-hooks';
+import { ZipkinExporter } from '@opentelemetry/exporter-zipkin';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { NodeTracerProvider } from '@opentelemetry/node';
-import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/tracing';
+import { BatchSpanProcessor } from '@opentelemetry/tracing';
 
 const contextManager = new AsyncHooksContextManager();
 contextManager.enable();
 
 const provider = new NodeTracerProvider();
+
+provider.addSpanProcessor(
+    new BatchSpanProcessor(
+        new ZipkinExporter({
+            serviceName: 'monolith',
+            url: 'http://localhost:9411/api/v2/spans',
+        }),
+    ),
+);
 
 provider.register({ contextManager });
 
