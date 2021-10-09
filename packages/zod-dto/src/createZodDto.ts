7a60dc6fb7b36a98902a1b9a20bde79826469c38
Jakub Kisielewski <6276426+kbkk@users.noreply.github.com>
2021-10-10 01:28:39 +0200

    zod-dto(chore): support zod =>3.8.0

--- packages/zod-dto/src/createZodDto.ts
+++ packages/zod-dto/src/createZodDto.ts
@@ -7,11 +7,27 @@ import * as z from 'zod';
  * Since we're interested in the main subset of Zod functionality (type infering + parsing) this type is introduced
  * to achieve the most compatibility.
  */
-export type CompatibleZodType = Pick<z.ZodType<unknown>, '_input' | '_output' | 'parse' | 'safeParse'>;
+export type CompatibleZodIssue = {
+    message: string;
+    path: (string | number)[];
+};
+export type CompatibleZodType = Pick<z.ZodType<unknown>, '_input' | '_output'> & {
+    parse: (...args) => unknown,
+    safeParse: (...args) => {
+        success: true;
+        data: unknown;
+    } | {
+        success: false;
+        error: {
+            issues: CompatibleZodIssue[];
+            errors: CompatibleZodIssue[];
+        }
+    },
+};
 export type CompatibleZodInfer<T extends CompatibleZodType> = T['_output'];
 
 export type ZodDtoStatic<T> = {
-    new (): T;
+    new(): T;
     zodSchema: CompatibleZodType;
     create(input: unknown): T;
 };
